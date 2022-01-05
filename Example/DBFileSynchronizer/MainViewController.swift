2e19092fa1b4f32e387aeee24147b1e7162c2009
Eddie Lau <eddie@touchutility.com>
2022-01-05 17:53:23 +0800

    cleaned up the example project.

--- Example/DBFileSynchronizer/MainViewController.swift
+++ Example/DBFileSynchronizer/MainViewController.swift
@@ -8,6 +8,8 @@
 
 import Foundation
 import UIKit
+import ObjectiveDropboxOfficial
+import DBFileSynchronizer
 
 class MainViewController : UIViewController {
     
--- Example/DBFileSynchronizer/SyncableText.swift
+++ Example/DBFileSynchronizer/SyncableText.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import DBFileSynchronizer
 
 class SyncableText : NSObject, DBSyncable {
     
